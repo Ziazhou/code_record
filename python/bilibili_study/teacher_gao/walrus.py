704afbbcc123d10453ea440493db0b339887b61e
azia.zhou <zia.zhou@outlook.com>
2025-08-23 20:35:37 +0800

    first file: walrus.py

---
+++ python/bilibili_study/teacher_gao/walrus.py
@@ -0,0 +1,29 @@
+l = [1, 2, 3]
+
+# length = len(l)
+# if length > 1:
+#   print(f"list is not empty")
+  
+  
+# if (length := len(l)) > 1:
+#   print(f"list is not empty")
+
+
+# region
+l = [2, 4, 6, 10]
+
+is_even = True
+
+# for i in l:
+#   if (i % 2) != 0:
+#     is_even = False
+#     break
+
+# is_even = all(i % 2 == 0 for i in l)
+
+# is_even = all((n:=i) % 2 == 0 for i in l)
+
+# print(is_even)
+# print(n)
+
+# endregion
